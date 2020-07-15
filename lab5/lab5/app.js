f215678f50ca52b8e1d616bc50093c55ed53384f
adamitdeka <adamitdeka@gmail.com>
2020-07-15 14:34:06 -0400

    first commit

---
+++ lab5/lab5/app.js
@@ -0,0 +1,20 @@
+
+window.onload=initfunction;
+function initfunction() {
+    var toDay = new Date();	
+    document.getElementById("dtfield").innerHTML = toDay;
+}
+
+function buttonandchecks() {
+    var x;
+	var message="";
+    var resultmessage="";
+    var radio_value; //get value from radio button
+    var totalvalue=parseInt(0);
+   
+
+
+
+
+
+}
